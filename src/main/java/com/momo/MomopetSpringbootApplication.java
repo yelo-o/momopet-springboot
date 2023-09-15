66355f0744924caa214c96ea941f5120e0c09eaf
yelo-o <flyordig@gmail.com>
2023-09-15 16:48:41 +0900

    초기설정

---
+++ src/main/java/com/momo/MomopetSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.momo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomopetSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomopetSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/momo/domain/Member.java
@@ -0,0 +1,4 @@
+package com.momo.domain;
+
+public class Member {
+}
