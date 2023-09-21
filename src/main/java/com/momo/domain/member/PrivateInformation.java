fbc5cc3c7fdc0e54edd7a77af7ad79643cfe1395
yelo-o <flyordig@gmail.com>
2023-09-21 12:04:40 +0900

    PrivateInformation에 name 속성 제외

--- src/main/java/com/momo/domain/member/PrivateInformation.java
+++ src/main/java/com/momo/domain/member/PrivateInformation.java
@@ -9,7 +9,6 @@ import java.time.LocalDateTime;
 @Embeddable
 @Getter
 public class PrivateInformation {
-    private String name;
     private LocalDateTime birthDate;
     private Gender gender; //FEMALE, MALE
     private Address address;
@@ -20,8 +19,7 @@ public class PrivateInformation {
     }
 
     //값 타입은 기본적으로 변경이 불가능하게 설계되어야 한다. -> 생성할 때만 값이 셋팅이 되고 setter는 제공하지 말아야 한다.
-    public PrivateInformation(String name, String email, LocalDateTime birthDate, Gender gender, Address address, String phoneNumber) {
-        this.name = name;
+    public PrivateInformation(String email, LocalDateTime birthDate, Gender gender, Address address, String phoneNumber) {
         this.birthDate = birthDate;
         this.gender = gender;
         this.address = address;
