df92e5138b92261b9fd741451a8adc54bd66a1f2
yelo-o <flyordig@gmail.com>
2023-09-21 11:50:48 +0900

    스프링 시큐리티 설정 파일 추가 및 User 클래스 구현

---
+++ src/main/java/com/momo/config/auth/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.momo.config.auth;
+
+import com.momo.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity  // Spring Security를 활성화
+//@Configuration
+public class SecurityConfig  extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable().headers().frameOptions().disable() //h2-console 화면을 위해 해당 옵션들 disable 처리
+            .and()
+                .authorizeHttpRequests() //URL별 권한 관리를 설정하는 옵션의 시작점
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll() //이쪽 url들은 아무런 권한없이 들어갈 수 있다.
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name()) //지정된 옵션에는 전체 열람 권한 부여 => 권한이 ROLE_USER인 경우
+                .antMatchers("/members/**").hasRole(Role.GUEST.name()) //지정된 옵션에는 전체 열람 권한 부여 => 권한이 ROLE_GUEST인 경우
+                .anyRequest().authenticated() //anyRequest() : 설정된 값들 이외 나머지 URL
+            .and()
+                .logout().logoutSuccessUrl("/") //로그아웃 기능에 대한 여러 설정의 진입점 & 로그아웃 성공 시 "/" 주소로 이동
+            .and()
+                .oauth2Login() //OAuth2 로그인 기능에 대한 여러 설정의 진입점
+                    .userInfoEndpoint() //OAuth2 로그인 성공 이후 사용자 정보 가져올 때의 설정 담당
+                        .userService(customOAuth2UserService); //소셜 로그인 성공 시 후속 조치를 진행할 UserService 인터페이스의 구현체 등록
+    }
+
+}
--- src/main/java/com/momo/controller/MemberController.java
+++ src/main/java/com/momo/controller/MemberController.java
@@ -1,25 +1,25 @@
 package com.momo.controller;
 
+import com.momo.config.auth.LoginUser;
+import com.momo.config.auth.dto.SessionUser;
 import com.momo.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class MemberController {
 
-    private final MemberService memberService;
-
-    @GetMapping("/members/selectMember")
-    public String selectMember() {
-        return "members/selectMember";
-
-    }
-
-    public String createForm(Model model) {
-        model.addAttribute("memberForm", new MemberForm());
-        return "members/createMemberForm";
+    @GetMapping("/members/myPage")
+    public String myPage(Model model, @LoginUser SessionUser user) {
+        log.info("로그인 이름 : " + user.getName());
+        if (user != null) {
+            model.addAttribute("user", user);
+        }
+        return "members/myPage";
     }
 }
