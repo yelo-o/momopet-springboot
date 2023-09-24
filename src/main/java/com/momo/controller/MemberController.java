f8bb890da77ff8142e64fb67436c6ffdd859bd29
yelo-o <flyordig@gmail.com>
2023-09-24 12:36:51 +0900

    마이페이지 구현
    - 소셜 로그인 후 마이페이지 접속 시, 개인정보 추가 입력 페이지로 이동
    - 추가입력이 완료되어야 마이페이지 이동 가능

--- src/main/java/com/momo/controller/MemberController.java
+++ src/main/java/com/momo/controller/MemberController.java
@@ -2,26 +2,92 @@ package com.momo.controller;
 
 import com.momo.config.auth.LoginUser;
 import com.momo.config.auth.dto.SessionUser;
+import com.momo.domain.member.Address;
+import com.momo.domain.member.Gender;
+import com.momo.domain.member.PrivateInformation;
+import com.momo.domain.user.Role;
+import com.momo.domain.user.User;
 import com.momo.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Controller
 @RequiredArgsConstructor
 @Slf4j
 public class MemberController {
 
-    @GetMapping("/members/myPage")
-    public String myPage(Model model, @LoginUser SessionUser user) {
-        log.info("로그인 이름 : " + user.getName());
-        if (user != null) {
-            model.addAttribute("user", user);
-        }
-        return "members/myPage";
+    private final MemberService memberService;
 
+    @GetMapping("/members/beSitter")
+    public String beSitterForm(Model model) {
+        model.addAttribute("memberUpdateForm", new MemberUpdateForm());
+        return "members/beSitterForm";
     }
+
+//    @GetMapping("/members/update")
+//    public String updateForm(Model model, @LoginUser SessionUser user) {
+//        log.info("로그인 이름 : " + user.getName());
+//        User findUser = memberService.findOne(user.getEmail());
+//
+//        model.addAttribute("user", findUser);
+//
+//        model.addAttribute("memberUpdateForm", new MemberUpdateForm());
+//        return "members/updateMemberForm";
+//    }
+
+    @PostMapping("/members/beSitter")
+    public String beSitter(@Valid MemberUpdateForm form, BindingResult result,
+                           @LoginUser SessionUser user, @RequestParam("gender") String genderString) {
+        //memberUpdateForm에 오류가 있는지 확인 (Validation)
+        if (result.hasErrors()) {
+            return "members/beSitterForm";
+        }
+
+        log.info("변환 전의 성별은 : " + genderString);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate birthDate = LocalDate.parse(form.getBirthDate() , formatter);
+        Address address = new Address(form.getSi() ,form.getGu());
+
+        Gender gender = null;
+        if (genderString.equals("여성")){
+            gender = Gender.FEMALE;
+        } else {
+            gender = Gender.MALE;
+        }
+
+        log.info("변환 이후의 성별은 : " + gender);
+
+        //개인정보 객체 생성
+        PrivateInformation privateInformation =
+                new PrivateInformation(birthDate, address, form.getPhoneNumber(), gender);
+
+        memberService.updateUser(user.getEmail(), privateInformation);
+
+        return "redirect:/members/myInfo";
+    }
+    @GetMapping("/members/myInfo")
+    public String myInfoForm(Model model, @LoginUser SessionUser user) {
+        User findUser = memberService.findOne(user.getEmail());
+        log.info("유저의 권한 : " + findUser.getRole());
+
+        if (findUser.getUserType() == null) {
+            return "redirect:/members/beSitter";
+        }
+        model.addAttribute("user", findUser);
+
+        return "members/myInfoForm";
+    }
+
 }
--- src/main/java/com/momo/domain/member/PrivateInformation.java
+++ src/main/java/com/momo/domain/member/PrivateInformation.java
@@ -2,15 +2,21 @@ package com.momo.domain.member;
 
 import com.momo.domain.member.Gender;
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Embeddable;
-import java.time.LocalDateTime;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import java.time.LocalDate;
 
 @Embeddable
-@Getter
+@Getter @Setter
 public class PrivateInformation {
-    private LocalDateTime birthDate;
+    private LocalDate birthDate;
+
+    @Enumerated(EnumType.STRING)
     private Gender gender; //FEMALE, MALE
+
     private Address address;
     private String phoneNumber;
 
@@ -19,10 +25,18 @@ public class PrivateInformation {
     }
 
     //값 타입은 기본적으로 변경이 불가능하게 설계되어야 한다. -> 생성할 때만 값이 셋팅이 되고 setter는 제공하지 말아야 한다.
-    public PrivateInformation(String email, LocalDateTime birthDate, Gender gender, Address address, String phoneNumber) {
+//    public PrivateInformation(String email, LocalDateTime birthDate, Gender gender, Address address, String phoneNumber) {
+//        this.birthDate = birthDate;
+//        this.gender = gender;
+//        this.address = address;
+//        this.phoneNumber = phoneNumber;
+//    }
+
+    public PrivateInformation(LocalDate birthDate, Address address, String phoneNumber, Gender gender) {
         this.birthDate = birthDate;
-        this.gender = gender;
         this.address = address;
         this.phoneNumber = phoneNumber;
+        this.gender = gender;
+
     }
 }
