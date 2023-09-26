fd02a45c89926f7a91ad6196e3929dc7d4e6ac49
ylshin92 <ylshin92@gmail.com>
2023-09-26 17:47:24 +0900

    Item 수정

--- src/main/java/com/momo/domain/Item.java
+++ src/main/java/com/momo/domain/Item.java
@@ -14,25 +14,45 @@ import java.time.LocalDateTime;
 @Table(name = "items")
 public class Item {
 
-    @Id
-    @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "item_id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
-
     private User sitter;
+
+    @Column
+    private String name;
+
+    @Column
+    private String email;
+
+    @Column
+    private String picture;
+
+    @Column
+    private String si;
+
+    @Column
+    private String gu;
 
     @Column(nullable = false)
     private int price; // 하루 일당
 
+    @Column
     private String introduction; //시터의 자기소개
 
+    @Column
     private LocalDateTime startDate;
+
+    @Column
     private LocalDateTime endDate;
 
-    private String dog;
-    private String cat;
+    @Column
+    private Boolean dog;
+
+    @Column
+    private Boolean cat;
 
 }
