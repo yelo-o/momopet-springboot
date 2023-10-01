406a7640f9265b4df87b688f88320db9fa121238
yelo-o <flyordig@gmail.com>
2023-10-02 00:12:25 +0900

    멤버 업데이트 리팩토링

--- src/main/java/com/momo/service/MemberService.java
+++ src/main/java/com/momo/service/MemberService.java
@@ -63,11 +63,10 @@ public class MemberService {
 //        memberRepository.save(findUser);
     }
 
-    public void updatePet(String name, Gender gender, PetType petType, String breed,
-                          LocalDate birthDate, String remark, Pet pet) {
+    public void updatePet(String name, Gender gender, PetType petType,
+                          String breed, LocalDate birthDate, String remark, Pet pet) {
+
         pet.update(name, gender, petType, breed, birthDate, remark);
-
-
     }
 
     //펫 정보 불러오기
@@ -81,9 +80,9 @@ public class MemberService {
     }
 
     //펫 추가
-    public void add(Pet pet, User findUser) {
+    public void add(Pet pet) {
         memberRepository.save(pet);
-        findUser.upgrade(); //SITTER => OWNER 업그레이드
+//        findUser.upgrade(); //SITTER => OWNER 업그레이드
     }
 
 }
