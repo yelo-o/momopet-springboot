8e1c5e69594739c5db3ee35496fe07f5e2fb1b18
choyonghun <43197549+choyonghun@users.noreply.github.com>
2023-10-10 14:57:11 +0900

    comment삭제, comment 재도전
    comment삭제, comment 재도전

--- src/main/java/com/momo/repository/UserRepository.java
+++ src/main/java/com/momo/repository/UserRepository.java
@@ -3,8 +3,11 @@ package com.momo.repository;
 import com.momo.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
+    List<Object> findByLoginId(String loginId);
 }
